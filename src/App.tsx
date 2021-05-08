ad93fe579fa94ae19a28b204849f6de4f41d5752
mikan3rd <hiroki.ota@zenkigen.jp>
2021-05-08 16:57:36 +0900

    chore: add video tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,45 @@
+import { css } from "@emotion/react";
 import React from "react";
+import { Button, Container, Segment } from "semantic-ui-react";
 
-function App() {
+const App: React.VFC = () => {
+  const videoRef = React.useRef<HTMLVideoElement>(null);
+
+  const [mediaStream, setMediaStream] = React.useState<MediaStream | null>(null);
+
+  const startVideo = async () => {
+    const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    setMediaStream(mediaStream);
+  };
+
+  const stopVideo = () => {
+    if (mediaStream) {
+      mediaStream.getTracks().forEach((track) => track.stop());
+    }
+  };
+
+  React.useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.srcObject = mediaStream;
+    }
+  }, [mediaStream]);
+
   return (
-    <div>
-      <header>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container
+      text
+      css={css`
+        &&& {
+          margin-top: 12px;
+        }
+      `}
+    >
+      <Button content="START" positive onClick={startVideo} />
+      <Button content="STOP" negative onClick={stopVideo} />
+      <Segment>
+        <video ref={videoRef} autoPlay />
+      </Segment>
+    </Container>
   );
-}
+};
 
 export default App;
