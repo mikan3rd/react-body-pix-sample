361a0e463d84373785a0d35f8cface260da88b63
mikan3rd <hiroki.ota@zenkigen.jp>
2021-05-08 22:32:41 +0900

    chore: bodyPixNetRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import "@tensorflow/tfjs";
 import { css } from "@emotion/react";
 import * as bodyPix from "@tensorflow-models/body-pix";
+import "@tensorflow/tfjs";
 import React from "react";
 import { Checkbox, Container, Segment } from "semantic-ui-react";
 
@@ -9,14 +9,36 @@ const height = 480;
 
 const App: React.VFC = () => {
   const videoRef = React.useRef<HTMLVideoElement>(null);
-  const requestAnimationId = React.useRef<number | null>(null);
+  const requestAnimationIdRef = React.useRef<number | null>(null);
+  const bodyPixNetRef = React.useRef<bodyPix.BodyPix | null>(null);
 
   // bodyPix.drawBokehEffect()でcanvasを破壊的に変更するためrefは使用しない
   // const canvasRef = React.useRef<HTMLCanvasElement>(null);
 
   const [mediaStream, setMediaStream] = React.useState<MediaStream | null>(null);
-  const [bodyPixNet, setBodyPixNet] = React.useState<bodyPix.BodyPix | null>(null);
-  const [useBodyPix, setUseBodyPix] = React.useState(false);
+
+  const useBodyPixRef = React.useRef(false);
+  const [useBodyPixState, setBodyPixState] = React.useState(false);
+
+  const renderCanvas = async () => {
+    const canvas = document.getElementById("canvas") as HTMLCanvasElement | null;
+    const video = videoRef.current;
+    const useBodyPix = useBodyPixRef.current;
+    const bodyPixNet = bodyPixNetRef.current;
+
+    if (canvas && video) {
+      if (useBodyPix && bodyPixNet) {
+        const segmentation = await bodyPixNet.segmentPerson(video);
+        bodyPix.drawBokehEffect(canvas, video, segmentation, 10);
+      } else {
+        const ctx = canvas.getContext("2d");
+        ctx?.drawImage(video, 0, 0);
+      }
+    }
+
+    const requestId = requestAnimationFrame(await renderCanvas);
+    requestAnimationIdRef.current = requestId;
+  };
 
   const startVideo = async () => {
     const mediaStream = await navigator.mediaDevices.getUserMedia({
@@ -27,6 +49,12 @@ const App: React.VFC = () => {
       audio: true,
     });
     setMediaStream(mediaStream);
+
+    if (videoRef.current) {
+      videoRef.current.srcObject = mediaStream;
+    }
+
+    await renderCanvas();
   };
 
   const stopVideo = () => {
@@ -34,14 +62,16 @@ const App: React.VFC = () => {
       mediaStream.getTracks().forEach((track) => track.stop());
       setMediaStream(null);
     }
-    if (requestAnimationId.current !== null) {
-      cancelAnimationFrame(requestAnimationId.current);
-      requestAnimationId.current = null;
+
+    const requestAnimationId = requestAnimationIdRef.current;
+    if (requestAnimationId !== null) {
+      cancelAnimationFrame(requestAnimationId);
+      requestAnimationIdRef.current = null;
     }
   };
 
   const handleChangeVideo = () => {
-    if (mediaStream) {
+    if (mediaStream !== null) {
       stopVideo();
     } else {
       startVideo();
@@ -49,50 +79,14 @@ const App: React.VFC = () => {
   };
 
   const handleBodyPix = async () => {
-    if (!useBodyPix && !bodyPixNet) {
+    const useBodyPix = useBodyPixRef.current;
+    if (!useBodyPix && !bodyPixNetRef.current) {
       const net = await bodyPix.load();
-      setBodyPixNet(net);
+      bodyPixNetRef.current = net;
     }
-    setUseBodyPix((prev) => !prev);
+    useBodyPixRef.current = !useBodyPix;
+    setBodyPixState(!useBodyPix);
   };
-
-  React.useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current.srcObject = mediaStream;
-    }
-  }, [mediaStream]);
-
-  React.useEffect(() => {
-    const renderCanvas = async () => {
-      const canvas = document.getElementById("canvas") as HTMLCanvasElement | null;
-      const video = videoRef.current;
-      if (canvas && video) {
-        if (useBodyPix && bodyPixNet) {
-          const segmentation = await bodyPixNet.segmentPerson(video, {
-            flipHorizontal: false, // ミラーリング
-            internalResolution: 0.35,
-            segmentationThreshold: 0.7,
-            maxDetections: 10,
-            scoreThreshold: 0.4,
-            nmsRadius: 20,
-          });
-          bodyPix.drawBokehEffect(canvas, video, segmentation, 5);
-        } else {
-          const ctx = canvas.getContext("2d");
-          ctx?.drawImage(video, 0, 0);
-        }
-      }
-
-      if (mediaStream) {
-        const requestId = requestAnimationFrame(renderCanvas);
-        requestAnimationId.current = requestId;
-      }
-    };
-
-    if (mediaStream) {
-      renderCanvas();
-    }
-  }, [bodyPixNet, mediaStream, useBodyPix]);
 
   return (
     <Container text>
@@ -108,7 +102,7 @@ const App: React.VFC = () => {
           margin-top: 12px;
         `}
       >
-        <Checkbox toggle checked={useBodyPix} label="BodyPix" onChange={handleBodyPix} />
+        <Checkbox toggle checked={useBodyPixState} label="BodyPix" onChange={handleBodyPix} />
       </div>
       <Segment>
         <video ref={videoRef} width={width} height={height} autoPlay hidden />
